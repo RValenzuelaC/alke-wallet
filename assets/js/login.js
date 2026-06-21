3b2a10ead84b6ed5ab898f9619c8b1d0d2d79f8f
Raul Valenzuela <rignacio.vcinto@gmail.com>
2026-06-21 01:59:12 -0400

    creación formulario y estilos + validación login

---
+++ assets/js/login.js
@@ -0,0 +1,41 @@
+const usuario = "Ignacio";
+const password = "1234";
+
+const btnSubmit = document.getElementById("submit");
+const usuarioRequerido = document.getElementById("usuario");
+const contrasenaRequerida = document.getElementById("password");
+const mensaje = document.getElementById("mensaje");
+
+btnSubmit.addEventListener("click", (e) => {
+	e.preventDefault();
+
+	const usuarioIngresado = usuarioRequerido.value.trim();
+	const contrasenaIngresada = contrasenaRequerida.value.trim();
+
+	if (usuarioIngresado === "" || contrasenaIngresada === "") {
+		mensaje.innerHTML = `
+	<div class="alert alert-warning">
+		Debes completar todos los campos
+	</div>
+`;
+		return;
+	}
+
+	if (usuarioIngresado === usuario && contrasenaIngresada === password) {
+		mensaje.innerHTML = `
+		<div class="alert alert-success">
+			Bienvenido
+		</div>
+	`;
+
+		setTimeout(() => {
+			window.location.href = "../menu/menu.html";
+		}, 1000);
+	} else {
+		mensaje.innerHTML = `
+	<div class="alert alert-danger">
+		Usuario o contraseña incorrectos
+	</div>
+`;
+	}
+});
